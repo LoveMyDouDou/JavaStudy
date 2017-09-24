f90cfcfea9be3c1f83e7c59545f81db9420fa54b
LoveMyDouDou <605988578@qq.com>
2017-09-24 19:21:02 +0800

    Initial commit

---
+++ src/Test17.java
@@ -0,0 +1,31 @@
+/**
+ * Created by YangGang on 2017/9/22.
+ */
+public class Test17 {
+    public static String output = " ";
+    public static void foo(int i)
+    {
+        try
+        {
+            if (i == 1)
+            {
+                throw new Exception();
+            }
+        }
+        catch (Exception e)
+        {
+            output +="2";
+            return ;
+        } finally
+        {
+            output +="3";
+        }
+        output +="4";
+    }
+    public static void main(String[] args)
+    {
+        foo(0);
+        foo(1);
+        System.out.println(output);
+    }
+}
